f0fd85c8f52194af4b3066faa7cedb604596f4b3
ws <ws@userdeimac-2.local>
2018-02-08 11:40:05 +0800

    添加 CZAdditions 通过桥接文件 让swift 支持oc代码

---
+++ sinaWeibo/sinaWeibo/sinaWeibo-Bridging.h
@@ -0,0 +1,14 @@
+//
+//  sinaWeibo-Bridging.h
+//  sinaWeibo
+//
+//  Created by ws on 2018/2/8.
+//  Copyright © 2018年 ws. All rights reserved.
+//
+
+#ifndef sinaWeibo_Bridging_h
+#define sinaWeibo_Bridging_h
+
+#import "CZAdditions.h"
+
+#endif /* sinaWeibo_Bridging_h */
